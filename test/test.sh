5dfc7dc3ba08854139df1d019d813ada6fdd5420
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2018-01-11 15:46:49 +0100

    update test.sh: remake and run test(s) in one command

--- test/test.sh
+++ test/test.sh
@@ -2,11 +2,15 @@
 
 # Primitive sanity-check testing: just make and try to connect to one hard-coded testnet peer.
 
-make > /dev/null # supresses STDOUT but not STDERR
+echo "Building..."
 
-if ( ./target/bcclient 148.251.191.74 -p 18333 | grep -q "Version received")
-then
-	echo "Tests OK"
-else
-	echo "Tests not passed!"
+# supress STDOUT but not STDERR
+if ( make remake > /dev/null ) then
+	echo "Build OK"
+	if ( ./target/bcclient 148.251.191.74 -p 18333 | grep -q "Version received") then
+		echo "Tests OK"
+	else
+		echo "Tests not passed!"
+	fi
 fi
+
