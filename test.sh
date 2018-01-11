cb06ff456b7f954b146b2dd2860ef3efb404ec9a
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2018-01-11 13:08:14 +0100

    add a simple testing script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Primitive sanity-check testing: just make and try to connect to one hard-coded testnet peer.
+
+make > /dev/null # supresses STDOUT but not STDERR
+
+if ( ./bcclient 148.251.191.74 -p 18333 | grep -q "Version received")
+then
+	echo "Tests OK"
+else
+	echo "Tests not passed!"
+fi
