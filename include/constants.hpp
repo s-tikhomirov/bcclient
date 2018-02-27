2e352034563a87ca6d2644610d1da0d71f1c9197
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2018-02-27 18:03:36 +0100

    remove port numbers from bcclient code (only in libbitcoin)

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,13 +2,9 @@
 #define CONSTANTS_HPP
 
 #include <cstdint>
+#include <bitcoin/constants.hpp>
 
-constexpr uint16_t BITCOIN_MAINNET_PORT = 8333;
-constexpr uint16_t BITCOIN_TESTNET_PORT = 18333;
-constexpr uint16_t ZCASH_MAINNET_PORT = 8233;
-constexpr uint16_t ZCASH_TESTNET_PORT = 18233;
-
-constexpr uint16_t DEFAULT_PORT = ZCASH_TESTNET_PORT;
+constexpr uint16_t DEFAULT_PORT = libbitcoin::protocol_port;
 
 constexpr uint16_t THREADS = 1;	// was 4
 
