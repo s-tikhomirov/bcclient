e2da21924cb4f3b8e42aa1d8b38ff4ccbc960316
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2018-03-20 14:37:12 +0100

    2nd (getaddr-addr) round yields +7% live peers

--- utils/get-live-peers.py
+++ utils/get-live-peers.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import socket
-import time
+import time, datetime
 import re
 from multiprocessing import Pool
 import itertools
@@ -41,7 +41,8 @@ def findIp(string):
 		return maybeIpv6
 	return maybeIpv4
 
-def get_addresses_from_peers(peers_filename, bcclient_path='../', timeout=15):
+def get_addresses_from_peers(peers_filename, bcclient_path='../', timeout=240):
+	print("Receiving addr from peers in " + peers_filename + "...")
 	lines = subprocess.check_output('timeout ' + str(timeout) + ' ./bcclient -f ' + peers_filename + ' -s getaddr -l addr | grep r:addr', 
 		shell=True,
 		cwd=bcclient_path).decode('utf-8').split('\n')
@@ -51,19 +52,20 @@ def get_addresses_from_peers(peers_filename, bcclient_path='../', timeout=15):
 			ips.append(findIp(line).group(0))
 		except ValueError:
 			continue
-	ips = list(set(ips))
+	ips = list(set(ips))	
+	print('Received ' + str(len(ips)) + ' distinct IPs.\n')
 	return ips
 
-def is_accessible(response):
+def is_live(response):
 	return (('Version received' in response) and ('Verack received' in response))
 
-def get_accessible_ips(lines, bcclient_path='../', timeout=1):
+def get_live_ips(ips, bcclient_path='../', timeout=1):
 	good_ips = []
 	bad_ips = []
-	total_ips = len(lines)
+	total_ips = len(ips)
 	print("Total " + str(total_ips) + " IPs in file.")
 	counter = 0
-	for ip in lines:
+	for ip in ips:
 		if (counter % 10 == 0):
 			print("Checking IP " + str(counter) + " of " + str(total_ips))
 		response = ''
@@ -73,59 +75,50 @@ def get_accessible_ips(lines, bcclient_path='../', timeout=1):
 				cwd=bcclient_path).decode('utf-8')
 		except subprocess.CalledProcessError:
 			pass	# ignore exit on timeout: peer is inaccessible
-		if is_accessible(response):
+		if is_live(response):
 			good_ips.append(ip)
 		else:
 			bad_ips.append(ip)
 		counter += 1
-
 	print(str(len(good_ips)) + ' accessible, ' + str(len(bad_ips)) + ' inaccessible IPs.')
 	return good_ips
 
-
-def func(index):
-  print('func: starting' + str(index))
-  for i in range(10000000): pass
-  print('func: finishing' + str(index))
-  return str(index)
-
-def multi_test():
-	pool = Pool(processes=4)
-	print(pool.map(func, range(4)))
+def get_live_ips_parallel(ips, n=32):
+	print("Checking liveness in " + str(n) + " threads...")
+	pool = Pool(processes=n)
+	ips_chunks = [ips[i::n] for i in range(n)]
+	print("Divided " + str(len(ips)) + " into " + str(len(ips_chunks)) + " chunks.")
+	live_ips = [pool.apply_async(get_live_ips, (ips_chunks[i], )) for i in range(n)]
+	print("Total: " + str(len(live_ips)) + " live peers.")
+	return [item for sublist in [res.get() for res in live_ips] for item in sublist]
 
 
 def main():
 	path = '/home/sergei/Documents/code/bcclient/bcclient/utils/'
-	bootstrap_urls_file = path + 'bootstrap-dns-bitcoin-testnet.txt'
-	bootstrap_ips_file = path + 'bootstrap-peers.txt'
-	all_peers_file = path + 'all-peers.txt'
-	good_peers_file = path + 'good-peers.txt'
-
-	test_peers_file = path + 'peers-test.txt'
-	bcclient_path = '../'
+	bootstrap_filename = path + 'bootstrap-dns-bitcoin-testnet.txt'
+	bootstrap_ips_file = path + 'bootstrap-peers_' + str(datetime.datetime.now().date()) +'.txt'
+	live_ips_file = path + 'live-peers_' + str(datetime.datetime.now().date()) +'.txt'
 	
 	# get boostrap peers
-	bootstrap_ips = get_ips_from_dns(get_bootstrap_urls(bootstrap_urls_file))
+	bootstrap_ips = get_ips_from_dns(get_bootstrap_urls(bootstrap_filename))
 	write_list_to_file(bootstrap_ips, bootstrap_ips_file)
 	
-	# get all peers
-	all_ips = get_addresses_from_peers(bootstrap_ips_file, bcclient_path, timeout=180)
-	print('Received ' + str(len(all_ips)) + ' distinct IPs.\n')
-	write_list_to_file(all_ips, all_peers_file)
-		
-	n = 32
-	#all_ips = [line.rstrip('\n') for line in open(path + 'good-peers-2018-03-17.txt', 'r')][1:]
-	all_ips_chunks = [all_ips[i::n] for i in range(n)]
-	print("Divided " + str(len(all_ips)) + " into " + str(len(all_ips_chunks)) + " chunks.")
+	# get addr from bootstrap peers
+	all_ips_1 = get_addresses_from_peers(bootstrap_ips_file)
 
-	pool = Pool(processes=n)
-	ips = [pool.apply_async(get_accessible_ips, (all_ips_chunks[i], )) for i in range(n)]
-	ips_final = [item for sublist in [res.get() for res in ips] for item in sublist]
-	#print(ips_final)
-	#ips = get_accessible_ips([line.rstrip('\n') for line in open(path + 'peers-test.txt', 'r')][1:])
-	write_list_to_file(ips_final, path + 'good-peers-2018-03-19.txt')
+	# filter live peers (round 1)
+	ips_live_1 = get_live_ips_parallel(all_ips_1)
+	write_list_to_file(ips_live_1, path + 'live-peers_1_' + str(datetime.datetime.now().date()) +'.txt')
+	
+	# get addr from all live peers
+	all_ips_2 = get_addresses_from_peers(path + 'live-peers_1_' + str(datetime.datetime.now().date()) +'.txt')
 
-	#multi_test()
+	# filter live peers (round 2)
+	ips_live_2 = get_live_ips_parallel([ip for ip in all_ips_2 if ip not in all_ips_1])
+	write_list_to_file(ips_live_2, path + 'live-peers_2_' + str(datetime.datetime.now().date()) +'.txt')
+
+	write_list_to_file(ips_live_1 + ips_live_2, live_ips_file)
+
 
 if __name__ == "__main__":
     main()
